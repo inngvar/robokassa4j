bddb8471c72fa8f2f40a341a1f3f57d705226621
igor.ch <igor.ch@sunriser-r.ru>
2012-12-23 20:46:01 +0400

    RequestFactory interface become abstract class

--- org.robo/src/main/java/org/robo/factory/RequestFactory.java
+++ org.robo/src/main/java/org/robo/factory/RequestFactory.java
@@ -7,7 +7,7 @@ import org.robo.data.RequestData;
  * @author Orphey
  *
  */
-public interface RequestFactory {
+public abstract class RequestFactory {
 	
 	/**
 	 * Create request for payment.
@@ -19,6 +19,6 @@ public interface RequestFactory {
 	 * @param description Description of the payment 
 	 * @return Data that will be sended to the robokassa service
 	 */
-	RequestData create(int invoceId, String mrchLogin, String sum2, String culture, String currency, String description);
+	public abstract RequestData create(int invoceId, String mrchLogin, String sum2, String culture, String currency, String description);
 
 }
